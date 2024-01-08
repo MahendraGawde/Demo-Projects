fe3904f44a800fbe077a0ee655a247101927d5dd
Mahendra <you@example.com>
2024-01-08 14:42:51 +0530

    ReverseString commit

---
+++ src/basics3/ReverseString.java
@@ -0,0 +1,16 @@
+package src.basics3;
+
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter String to reverse: ");
+        String inputUser = sc.next();
+
+
+        StringBuilder sb1= new StringBuilder(inputUser);
+        StringBuilder reverse = sb1.reverse();
+        System.out.println("Reverse string is: " +reverse);
+    }
+}
