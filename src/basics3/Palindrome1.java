c0692087fd44b2ff9976f2313c0fb13e5e825759
Mahendra <you@example.com>
2024-01-08 20:06:31 +0530

    Palindrome exercise Updated

---
+++ src/basics3/Palindrome1.java
@@ -0,0 +1,25 @@
+package src.basics3;
+
+
+import java.util.Scanner;
+
+public class Palindrome1 {
+    public static void main(String[] args) {
+        // Palindrome with StringBuilder class
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter text to check palindrome or not");
+        String inputUser = sc.next();
+        inputUser = inputUser.toLowerCase();
+
+        StringBuilder sb1 = new StringBuilder(inputUser);
+       // StringBuilder reverseString = sb1.reverse().toString();
+        StringBuilder reverseString = new StringBuilder(sb1.reverse().toString());
+
+        if (inputUser.contentEquals(reverseString) ) {
+            System.out.println("This is palindrome");
+
+        }else {
+            System.out.println("This is not a palindrome");
+        }
+    }
+}
