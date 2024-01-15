ebb6067eb0ec2191af66d2b2433669530126bafb
Mahendra <you@example.com>
2024-01-15 19:14:50 +0530

    Block class and Runner class

---
+++ src/OOPsPractice/IIB/BlockClass.java
@@ -0,0 +1,15 @@
+package src.OOPsPractice.IIB;
+
+public class BlockClass {
+
+    BlockClass(){
+        System.out.println("Default Constructor IIB..");
+    }
+    //Instance Initialization Block
+    {
+        System.out.println("This is Instance Initialization Block");
+    }
+    static{
+        System.out.println("Static IB");
+    }
+}
---
+++ src/OOPsPractice/IIB/RunnerClass.java
@@ -0,0 +1,8 @@
+package src.OOPsPractice.IIB;
+
+public class RunnerClass {
+    public static void main(String[] args) {
+        new BlockClass();
+        new BlockClass();
+    }
+}
